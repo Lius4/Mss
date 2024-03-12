10001158e8bf4746fd6949e8c25f7bdf09746d2f
Lius4 <122825952+Lius4@users.noreply.github.com>
2024-03-12 15:48:53 +0300

    Add files via upload

---
+++ webkit/res/vanilla-animate/script.js
@@ -0,0 +1,19 @@
+VanillaTilt.init(document.querySelector(".swags"), {
+	max: 10,
+	speed: 400
+});
+
+VanillaTilt.init(document.querySelectorAll(".swags .row1"), {
+	max: 10,
+	speed: 400
+});
+
+VanillaTilt.init(document.querySelectorAll(".infos"), {
+	max: 20,
+	speed: 600
+});
+
+VanillaTilt.init(document.querySelectorAll(".infos1"), {
+	max: 20,
+	speed: 600
+});
